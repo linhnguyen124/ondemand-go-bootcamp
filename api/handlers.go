c519f8addcb17a038cf7ed2917c2728800c90901
Linh Nguyen <linh.nguyen@wizeline.com>
2023-06-28 10:34:13 +0700

    add new file

--- api/handlers.go
+++ api/handlers.go
@@ -71,3 +71,83 @@ func readPokemonCSV(filePath string) ([]*model.Pokemon, error) {
 
 	return pokemonList, nil
 }
+
+// func NewPokemonAPI(baseURL string) *model.PokemonAPI {
+// 	return &model.PokemonAPI{BaseURL: baseURL}
+// }
+
+// func (c *model.PokemonAPI) GetPokemonByID(id int) (*model.Pokemon, error) {
+// 	url := fmt.Sprintf("%s/pokemon/%d", c.BaseURL, id)
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != http.StatusOK {
+// 		return nil, fmt.Errorf("failed to retrieve Pokemon: %s", resp.Status)
+// 	}
+
+// 	pokemon := &model.Pokemon{}
+// 	err = json.NewDecoder(resp.Body).Decode(pokemon)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return pokemon, nil
+// }
+
+// func StorePokemonInCSV(pokemon *model.Pokemon, filePath string) error {
+// 	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer file.Close()
+
+// 	writer := csv.NewWriter(file)
+// 	defer writer.Flush()
+
+// 	record := []string{strconv.Itoa(pokemon.ID), pokemon.Name}
+// 	err = writer.Write(record)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+// func GetPokemonByIDByExternal(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	idStr := vars["id"]
+
+// 	id, err := strconv.Atoi(idStr)
+// 	if err != nil {
+// 		http.Error(w, "Invalid ID", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	api := NewPokemonAPI("https://pokeapi.co/api/v2")
+// 	pokemon, err := api.GetPokemonByID(id)
+// 	if err != nil {
+// 		log.Println("Error retrieving Pokemon:", err)
+// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	err = StorePokemonInCSV(pokemon, "./resources/pokemon.csv")
+// 	if err != nil {
+// 		log.Println("Error storing Pokemon data in CSV:", err)
+// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// Display the result as JSON
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(pokemon)
+// }
+
+// func main() {
+// 	r := mux.NewRouter()
+// 	r.HandleFunc("/pokemon/{id}", GetPokemonByIDByExternal).Methods("GET")
+// 	log.Fatal(http.ListenAndServe(":8080", r))
+// }
---
+++ model/pokemonAPI.go
@@ -0,0 +1,5 @@
+package model
+
+type PokemonAPI struct {
+	BaseURL string
+}
